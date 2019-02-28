b26bf0bae093fc4ca54891237ae9f15d8e9104f5
Feuillade Julien <julien.feuillad@he-arc.ch>
2019-02-28 15:59:39 +0100

    login

--- rudumb/rudumbapp/urls.py
+++ rudumb/rudumbapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.views.generic.base import TemplateView # new
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
