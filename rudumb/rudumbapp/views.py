60b9768bb47443921233404749f5aa5b2ffb4b6a
jules.perrelet1 <jules.perrelet@he-arc.ch>
2019-03-06 13:59:24 +0100

    index page

--- rudumb/rudumbapp/views.py
+++ rudumb/rudumbapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from .models import Quizzes 
 
 
 # Create your views here.
 def index(request):
     context = {}
+    quizz_list = Quizzes.objects.all()
+
     return render(request, 'rudumbapp/index.html', context)
+
