e2361927cec8dfa59fa98f5c196148f1e5742196
Royejul <julien.feuillad@he-arc.ch>
2019-03-31 19:00:14 +0200

    remove useless method in forms.py file

--- rudumb/rudumbapp/forms.py
+++ rudumb/rudumbapp/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from django.forms import (formset_factory, modelformset_factory)
-
-from .models import (Quiz, Question, Category)
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(
@@ -21,59 +18,3 @@ class UserRegistrationForm(forms.Form):
         max_length=32,
         widget=forms.PasswordInput()
     )
-
-class BookForm(forms.Form):
-    name = forms.CharField(
-        label='Book Name',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Book Name here'
-        })
-    )
-
-
-class BookModelForm(forms.ModelForm):
-
-    class Meta:
-        model = Quiz
-        fields = ('name', )
-        labels = {
-            'name': 'Book Name'
-        }
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter Book Name here'
-                }
-            )
-        }
-
-
-BookFormset = formset_factory(BookForm)
-BookModelFormset = modelformset_factory(
-    Quiz,
-    fields=('name', ),
-    extra=1,
-    widgets={
-        'name': forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Book Name here'
-            }
-        )
-    }
-)
-
-AuthorFormset = modelformset_factory(
-    Question,
-    fields=('question', 'choice1'),
-    extra=2,
-    widgets={'question': forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Author Name here'
-        }),
-            'choice1': forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Author Name here'
-        })
-    }
-)
