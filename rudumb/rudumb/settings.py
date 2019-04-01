a71daafc852463e4d39d74340ab823dc21511a2d
Royejul <julien.feuillad@he-arc.ch>
2019-04-01 23:07:30 +0200

    Settings database

--- rudumb/rudumb/settings.py
+++ rudumb/rudumb/settings.py
@@ -91,17 +91,14 @@ WSGI_APPLICATION = 'rudumb.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.environ.get('GROUPNAME'),
-    'USER': os.environ.get('GROUPNAME', 'root'),
-    'PASSWORD': os.environ.get('PASSWORD', ''),
-    'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-    'PORT': os.environ.get('MYSQL_PORT', '3306'),
-    'OPTIONS': {
-      'charset': 'utf8mb4'
-    }
-  }
+   'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'rudumb',
+      'USER': 'root',
+      'PASSWORD': '',
+      'HOST': '127.0.0.1',
+      'PORT': '3306',
+   }
 }
 
 # DATABASES = {
