6ad624323819f540b234d0d549dd8f2cec7ff707
Royejul <julien.feuillad@he-arc.ch>
2019-04-01 18:07:09 +0200

    Problem favicon

--- rudumb/rudumb/urls.py
+++ rudumb/rudumb/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('rudumbapp.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.MEDIA_URL + 'favicon.ico')),
     url(r'^admin/', admin.site.urls),
     url(r'^login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     url(r'^logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
