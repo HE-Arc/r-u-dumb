6a31b7f1d81bf1298005af38e4960a41be99cfc2
Royejul <julien.feuillad@he-arc.ch>
2019-04-01 22:56:12 +0200

    Change database login

--- rudumb/rudumb/settings.py
+++ rudumb/rudumb/settings.py
@@ -91,16 +91,30 @@ WSGI_APPLICATION = 'rudumb.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rudumbdb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
-
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.environ.get('GROUPNAME'),
+    'USER': os.environ.get('GROUPNAME', 'root'),
+    'PASSWORD': os.environ.get('PASSWORD', ''),
+    'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+    'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    'OPTIONS': {
+      'charset': 'utf8mb4'
     }
+  }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'rudumbdb',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         #'HOST': '127.0.0.1',
+#         #'PORT': '5432',
+#
+#     }
+# }
 
 
 # Password validation
