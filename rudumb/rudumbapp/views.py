34e8c5057c1ce4606c90a4d235e78c8c24a6ceb3
ErwanBueche <erwan.bueche@he-arc.ch>
2019-04-01 17:46:25 +0200

    connection a la db et classements affichés

--- rudumb/rudumbapp/views.py
+++ rudumb/rudumbapp/views.py
@@ -9,7 +9,7 @@ from .static.fusioncharts import FusionCharts
 
 from collections import OrderedDict
 
-from .models import Quiz, Category
+from .models import Quiz, Category, Stat, AuthUser
 
 
 # Create your views here.
@@ -53,40 +53,8 @@ def dashboard(request):
     chartConfig["xAxisName"] = "Score"
     chartConfig["yAxisName"] = "Number of players at that score"
     chartConfig["theme"] = "fusion"
-
-    # The `chartData` dict contains key-value pairs data
-    chartData = OrderedDict()
-    chartData["0"] = 30
-    chartData["1"] = 45
-    chartData["2"] = 54
-    chartData["3"] = 70
-    chartData["4"] = 140
-    chartData["5"] = 167
-    chartData["6"] = 234
-    chartData["7"] = 256
-    chartData["8"] = 189
-    chartData["9"] = 165
-    chartData["10"] = 89
-
-
     dataSource["chart"] = chartConfig
     dataSource["data"] = []
-    
-    # Convert the data in the `chartData` array into a format that can be consumed by FusionCharts. 
-    # The data for the chart should be in an array wherein each element of the array is a JSON object
-    # having the `label` and `value` as keys.
-
-    # Iterate through the data in `chartData` and insert in to the `dataSource['data']` list.
-    for key, value in chartData.items():
-        data = {}
-        data["label"] = key
-        data["value"] = value
-        dataSource["data"].append(data)
-
-
-    # Create an object for the column 2D chart using the FusionCharts class constructor
-    # The chart data is passed to the `dataSource` parameter.
-    column2D = FusionCharts("column2d", "ex1", "100%", "30%", "distribution_chart", "json", dataSource)
 
     dummy_data = [
         {
@@ -109,15 +77,98 @@ def dashboard(request):
         }
     ]
 
+
+    current_user = request.user.id
+    n_quiz = Stat.objects.all().count()
+    n_done = passed_quiz = Stat.objects.filter(user=current_user).count()
+
+    #Calcul de la moyenne de l'utilisateur courant
+    passed_quiz = Stat.objects.filter(user=current_user)
+
+    #Compteur
+    player_total_score = 0
+    for quiz in passed_quiz.iterator():
+        player_total_score += quiz.result_quiz
+
+    #Division par zéro
+    if n_quiz == 0:
+        ratio = "x/0"
+    else:
+        ratio = n_done/n_quiz*100
+
+
+    #Calcul de la moyenne globale des autres utilisateurs
+    passed_quiz_by_others = Stat.objects.exclude(user=current_user)
+
+    #Compteurs
+    others_total_score = 0
+    others_n = 0
+    for quiz in passed_quiz_by_others.iterator():
+        others_total_score += quiz.result_quiz
+        others_n += 1
+
+    #Division par zéro
+    if others_n == 0:
+        others_average = "x/0"
+    else:
+        others_average = others_total_score/others_n
+
+    # Populate the gaussian
+    users = AuthUser.objects.all()
+
+    # Creer dict
+    gaussian = dict()
+    total_score = 0
+    # Pour chaque utilisateur
+    for user in users.iterator():
+        user_points = 0
+        quiz_user = Stat.objects.filter(user=user)
+        # Compte ses points totauxs
+        for quiz in quiz_user.iterator():
+            user_points += quiz.result_quiz
+        # Enter data in gaussian dict
+        if user_points in gaussian:
+            gaussian[user_points] += 1
+        else:
+            gaussian[user_points] = 1
+        
+        # Remember the user score
+        if user == current_user:
+            total_score = user_points
+
+    #Sort the gaussian and fill it with the zeros
+    right_limit = max(gaussian.keys())
+    for i in range(0, right_limit):
+        # Fill in the blanks
+        if i not in gaussian:
+            gaussian[i] = 0
+
+    
+    # Convert the data in the `chartData` array into a format that can be consumed by FusionCharts. 
+    # The data for the chart should be in an array wherein each element of the array is a JSON object
+    # having the `label` and `value` as keys.
+    # Iterate through the data in `incomplete_gaussian` and insert in to the `dataSource['data']` list.
+    for key, value in gaussian.items():
+        data = {}
+        data["label"] = key
+        data["value"] = value
+        if key == total_score:
+            data["color"] = "#FF4500" #Custom Color
+        dataSource["data"].append(data)
+
+    # Create an object for the column 2D chart using the FusionCharts class constructor
+    # The chart data is passed to the `dataSource` parameter.
+    column2D = FusionCharts("column2d", "ex1", "100%", "30%", "distribution_chart", "json", dataSource)
+
     scores = {
-        'total_completed': '37',
-        'global_ratio': '78%',
-        'completed_ratio': '60%',
-        'global_leaderboard': '10/2190'
+        'total_completed': n_done,
+        'completed_ratio': ratio,
+        'other_players_avg': others_average,
+        'total_score': total_score
     }
 
     context = {
-        'historic': dummy_data,
+        'historic': passed_quiz,
         'score': scores,
         'distribution': column2D.render()
     }
