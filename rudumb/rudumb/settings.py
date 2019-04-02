1a695b8bab44c5ad86c14a5afc38f4feca0f2ef6
Royejul <julien.feuillad@he-arc.ch>
2019-04-02 10:25:27 +0200

    settings

--- rudumb/rudumb/settings.py
+++ rudumb/rudumb/settings.py
@@ -91,18 +91,25 @@ WSGI_APPLICATION = 'rudumb.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.environ.get('GROUPNAME'),
-    'USER': os.environ.get('GROUPNAME', 'root'),
-    'PASSWORD': os.environ.get('PASSWORD', ''),
-    'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-    'PORT': os.environ.get('MYSQL_PORT', '3306'),
-    'OPTIONS': {
-      'charset': 'utf8mb4'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-  }
 }
+
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': os.environ.get('GROUPNAME'),
+#     'USER': os.environ.get('GROUPNAME', 'root'),
+#     'PASSWORD': os.environ.get('PASSWORD', ''),
+#     'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+#     'PORT': os.environ.get('MYSQL_PORT', '3306'),
+#     'OPTIONS': {
+#       'charset': 'utf8mb4'
+#     }
+#   }
+# }
 
 # DATABASES = {
 #     'default': {
